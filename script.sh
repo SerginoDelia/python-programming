0af0c9d43a73df13de3157f580bf54c7cd81d101
Sergino Delia <sergino.co@gmail.com>
2024-09-01 18:31:05 -0400

    added updates

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
+echo Adding the fix to ~/.zshrc
+echo export DOCKER_DEFAULT_PLATFORM=linux/amd64 >> ~/.zshrc
+echo export DOCKER_BUILDKIT=0 >> ~/.zshrc
+
+
 i=0
 
-while [ $i -le 10 ];
+while [ $i -le 4 ];
 do
-echo It worked
+echo SSBsb3ZlIExpenpvIHdpdGggYWxsIG15IEhlYXJ0Cg== | base64 --decode
 ((i))
 done
