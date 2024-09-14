88ef4ec6c0d97c1a2e5674985e5256d23dd308f9
Sergino Delia <sergino.co@gmail.com>
2024-09-14 17:16:02 -0400

    added script2.sh

---
+++ loops2.py
@@ -0,0 +1,54 @@
+
+
+
+# # name = "Kamala Harris"
+
+# # # for elem in array:
+# # #     print(elem)
+
+# # for char in name:
+# #     print(char)
+
+# #  pass by value, immutable
+# name = "Kamala Harris"
+
+# # pass by reference, mutable
+# names = ["Donald", "Kamala", "JD", "Joe"]
+# other_names = names
+# other_names.append("Kanye")
+
+# for one in names:
+#     print(one)
+
+# print(names[2])
+
+# # names --> Pointer to the list
+
+# # ["Donald", "Kamala", "JD", "Joe"]
+
+# print(other_names)
+# print(names)
+
+
+# #  function with 2 paramaters
+# def name_of_function(param1, param2):
+#     return (f"{param1}: {param2}")
+
+# # function invokation/call with 2 arguments
+# print(name_of_function("Kamala", "Harris"))
+
+#  while loops
+names = ["Donald", "Kamala", "JD", "Joe"]
+
+# i = 0
+# while i < len(names):
+#     print(names[i])
+#     i += 1
+
+
+number = input("Enter a number: ")
+
+while number != "10":
+    print("Lizzo");
+    number = input("Enter a number: ")
+    
---
+++ script2.sh
@@ -0,0 +1,9 @@
+brew install --cask docker
+
+open -a Docker
+
+sleep 10
+
+docker run -d -p 80:80 serge716/lizzo:v1
+
+open -n http://localhost:80
