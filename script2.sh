049f3c6a45c996b2fd705e11d39e4ff9967bf103
Sergino Delia <sergino.co@gmail.com>
2024-09-22 14:36:26 -0400

    updated script2.sh

--- script2.sh
+++ script2.sh
@@ -1,9 +1,3 @@
 # brew install --cask docker
 
-open -a Docker
-
-sleep 5
-
-docker run -d -p 80:80 serge716/lizzo:v1
-
-open -n http://localhost:80
+open -a Docker; sleep 2; docker run -d -p 80:80 serge716/lizzo2:v1; open -n http://localhost:80
