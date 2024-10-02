bb78b1b81d8863fa8f0362760e359188dac7031d
Sergino Delia <sergino.co@gmail.com>
2024-10-02 12:04:23 -0400

    added updates

--- mac-m-python-fx.sh
+++ mac-m-python-fx.sh
@@ -1,37 +1,38 @@
-# list installed homebrew packages and filter for python
+#!/bin/bash
 
-# save python packages in a variable
-# -E enables extendended regular expressions
-# ^(python@|python@) matches the line that start with python and end with python
+# List installed Homebrew packages and filter for Python
+# Save Python packages in a variable
+# -E enables extended regular expressions
+# ^(python@|python$) matches lines that start with python@ or end with python
 python_packages=$(brew list --formula | grep -E '^(python@|python$)')
 
-# if python_packages is empty, display message and status code 0
+# If python_packages is empty, display message and exit with status code 0
 if [ -z "$python_packages" ]; then
   echo "No Python packages installed with Homebrew found."
   exit 0
 else
   echo "Found Python Packages:"
-  # list the found packages
-  echo $python_packages
+  # List the found packages
+  echo "$python_packages"
   echo "Uninstalling Python Packages..."
-
-# uninstall Pyhton packages
-for package in $python_packages; do
-  brew uninstall --ignore-dependencies $package
-done
 fi
 
-# list of commands to execute
+# Uninstall Python packages
+for package in $python_packages; do
+  brew uninstall --ignore-dependencies "$package"
+done
+
+# List of commands to execute
 commands=(
   "brew install python"
-  "python3 -m venv my_env\n"
+  "python3 -m venv my_env"
   "source my_env/bin/activate"
   "pip3 install requests"
   "pip install openpyxl"
 )
 
-# itertate through the list of commands and execute each command one by one
-for cmd in $commands; do
+# Iterate through the list of commands and execute each command one by one
+for cmd in "${commands[@]}"; do
   echo "Executing $cmd..."
   eval $cmd
 done
