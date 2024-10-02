9e7e284a5af5f5f7128ddae24751af1f64639a89
Sergino Delia <sergino.co@gmail.com>
2024-10-02 12:19:36 -0400

    added updates

--- mac-m-python-fx.sh
+++ mac-m-python-fx.sh
@@ -6,21 +6,19 @@
 # ^(python@|python$) matches lines that start with python@ or end with python
 python_packages=$(brew list --formula | grep -E '^(python@|python$)')
 
-# If python_packages is empty, display message and exit with status code 0
+# If python_packages is empty, display message and continue with the rest of the script
 if [ -z "$python_packages" ]; then
   echo "No Python packages installed with Homebrew found."
-  exit 0
 else
   echo "Found Python Packages:"
   # List the found packages
   echo "$python_packages"
   echo "Uninstalling Python Packages..."
+  # Uninstall Python packages
+  for package in $python_packages; do
+    brew uninstall --ignore-dependencies "$package"
+  done
 fi
-
-# Uninstall Python packages
-for package in $python_packages; do
-  brew uninstall --ignore-dependencies "$package"
-done
 
 # List of commands to execute
 commands=(
