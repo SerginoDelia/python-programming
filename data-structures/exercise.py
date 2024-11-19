1d6950f2186b1af05f5e7e140b2be189b84a180e
Sergino Delia <sergino.co@gmail.com>
2024-11-18 19:56:45 -0500

    added more notes

---
+++ data-structures/exercise.py
@@ -0,0 +1,57 @@
+# Write a program to find the most repeated character of the sentence below
+# import pretty printing
+from pprint import pprint
+
+sentence = "This is a common interview question"
+
+# P = write a program that return the character repeated the most
+# E = this is = i
+# D = input: string, output: string
+# A = remove white space, convert the sentence to an object key(letter) value(occurance),
+#     iterate through the dictioanry and return the highest value
+
+sentence.strip()
+letters = {}
+ 
+for char in sentence:
+  if char in letters:
+    letters[char] += 1
+  else:
+    letters[char] = 1
+
+result = sentence[0]
+
+for k in letters:
+  if letters[k] > letters[result]:
+    result = k
+
+print(result, letters[result])
+print(f"{result}, {letters[result]} times")
+
+# refactored
+char_frequency = {}
+for char in sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+      char_frequency[char] = 1
+pprint(char_frequency, width=1)
+
+# this method returns all the key, value pairs as Tuples
+char_frequency_sorted = sorted(
+    char_frequency.items(), 
+    key=lambda kv:kv[1], 
+    reverse=True)
+    
+print(char_frequency_sorted[0])
+
+
+
+
+
+
+
+
+
+
+
---
+++ data-structures/lambdas.py
@@ -0,0 +1,28 @@
+# Lambdas
+
+# Lambdas are one line functions, also know as anonymous functions in other 
+# programming languages. Use Lambdas when you don't want to use a function twice
+# 3 in a program, they are just like normal functions and behave like them
+
+# Syntax
+# lambda argument: manipulate(argument)
+
+# Example
+add = lambda x, y: x + y
+
+print(add(3, 5))
+# Output 8
+
+# Few useful cases for lambdas
+
+# List Sorting
+a = [(1, 2), (4, 1), (9,10), (13, -3)]
+a.sort(key=lambda x: x[1])
+
+print(a)
+# Output [(13, -30, (4, 1), (1, 2), (9, 10)]
+
+# Parallel sorting of lists
+data = zip(list1, list2)
+data = sorted(data)
+list1, list2 = map(lambda t: list(t), zip(*data))
