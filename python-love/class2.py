0f1c3e3dacaeb5db89556f0512a0ae663f03ae4b
Sergino Delia <sergino.co@gmail.com>
2025-01-05 10:56:04 -0500

    added more notes

---
+++ python-love/class2.py
@@ -0,0 +1,62 @@
+# write a  progragram that returns Fizz if a number is divisible by 3
+# if the number is divisible by Buzz
+# if the number is divible by 3 and 5 FizzBuzz
+# Problem
+#   if the number is divisble by 3 return Fizz
+#   if number is divisble by 5 return Buzz
+#   if number is divisble by 3 and 5 return FizzBuzz
+# Example Ouput:
+    # 15 --> FizzBuzz
+    # 10 --> Buzz
+    # 9 --> Fizz
+# Data:
+    # strings (input)
+    # boolean
+    # float/integer
+# Algorithm
+    # Write a helper function called fizzbuzz, that ask the user for an input (num) and
+    # convert it to a integer or float
+def fizzbuzz():
+    # num = int(input('Enter a number: '))
+    num = input('Enter a numebr: ')
+    num = int(num) # converts the input string to integer
+    # if num is divisible by 3 and 5 print 'FizzBuzz'
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    # if num is divisble by 3 print 'Fizz'
+    elif num % 3 == 0:
+        print('Fizz')
+    # if num is divisible by 5 print 'Buzz'
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(f'{num} is not divible by 3 or 5')
+
+fizzbuzz()
+
+# get number from the user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num1 = input('Enter a number: ')
+# num2 = input('Enter another numebr: ')
+
+# if num1 == num2:
+#     print(f'{num1} is greater than {num2}')
+# elif num1 < num2:
+#     print(f'{num1} is less than {num2}')
+# else:
+#     print(f'{num1} is greater than {num2}')
